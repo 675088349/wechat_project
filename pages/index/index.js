b0e5be981b9cc68e46e34498dd9caedd3712d177
DESKTOP-BT8EUS2\Administrator <675088349@qq.com>
2021-08-01 23:28:07 +0800

    no message

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,15 +6,105 @@ const app = getApp()
 
 Page({
   data: {
-    show: true
+    show: true,
+    curPage: 1,
+    pageSize: 20,
   },
  
   onLoad() {
-
     this.initBanners();
+    this.goodsDynamic();
+    this.categories();
+    this.getNotice();
   },
 
-  async initBanners(){
+  getNotice () {
+    var that = this
+    WXAPI.noticeList({pageSize:5}).then(function (res) {
+      if (res.code == 0) {
+        that.setData({
+          noticeList: res.data
+        });
+        console.log ('noticeList', res.data)
+      }
+    })
+  },
+
+  async categories() {
+    const res = await WXAPI.goodsCategory()
+    let categories = [];
+
+    if (res.code == 0) {
+      categories = res.data.filter(ele => {
+        return ele.level == 1
+      })
+    }
+    console.log('categories',categories)
+
+    this.setData ({
+      categories: categories,
+      activeCategoryId: 0,
+      curPage: 1
+    })
+
+    this.getGoodsList(0);
+  },
+
+  async getGoodsList (categoryId, append) {
+    if (categoryId == 0) {
+      categoryId = "";
+    }
+
+    wx.showLoading({
+      "mask": true
+    })
+
+    const res = await WXAPI.goods({
+      categoryId: categoryId,
+      page: this.data.curPage,
+      pageSize: this.data.pageSize
+    })
+
+    wx.hideLoading()
+    if (res.code == 404 || res.code == 700) {
+      let newData = {
+        loadingMoreHidden: false
+      }
+
+      if (!append) {
+        newData.goods = []
+      }
+
+      this.setData(newData);
+      return
+    }
+
+    var goods = [];
+    if (append) {
+      goods = this.data.goods;
+    }
+
+    for (var i = 0; i < res.data.length; i++) {
+      goods.push(res.data[i]);
+    }
+    console.log ('goods', goods)
+
+    this.setData ({
+      loadingMoreHidden: true,
+      goods: goods,
+    })
+  },
+
+  async goodsDynamic () {
+    const res = await WXAPI.goodsDynamic(0)
+    if (res.code == 0) {
+      this.setData ({
+        goodsDynamic: res.data
+      })
+    }
+  },
+
+  async initBanners() {
     const res = await WXAPI.banners({
       type: 'index'
     })
@@ -34,8 +124,6 @@ Page({
   },
 
   onShow() {
-
-    console.log("App.globalData.navHeight", app.globalData.navHeight)
 
     this.setData ({
       navHeight: app.globalData.navHeight,
